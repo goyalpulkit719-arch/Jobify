cbb6214463a08936cdd2b09a249a5d449b914d49
pulkit-first <goyalpulkit719@gmail.com>
2026-07-14 00:33:37 +0530

    Allow frontend origin in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const authRoutes = require('./routes/auth.routes');
-const companyRoutes = require('./routes/company.routes');
-const jobRoutes = require('./routes/job.routes');
-const candidateRoutes = require('./routes/candidate.routes');
-const applicationRoutes = require('./routes/application.routes');
+const authRoutes = require("./routes/auth.routes");
+const companyRoutes = require("./routes/company.routes");
+const jobRoutes = require("./routes/job.routes");
+const candidateRoutes = require("./routes/candidate.routes");
+const applicationRoutes = require("./routes/application.routes");
 
 const app = express();
 
@@ -14,17 +14,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://jobify-nwop.vercel.app"],
     credentials: true,
-}));
+  }),
+);
 
-
-app.use('/auth', authRoutes);
-app.use('/company', companyRoutes);
-app.use('/job', jobRoutes);
-app.use('/candidate', candidateRoutes);
-app.use('/application', applicationRoutes);
-
+app.use("/auth", authRoutes);
+app.use("/company", companyRoutes);
+app.use("/job", jobRoutes);
+app.use("/candidate", candidateRoutes);
+app.use("/application", applicationRoutes);
 
 module.exports = app;
